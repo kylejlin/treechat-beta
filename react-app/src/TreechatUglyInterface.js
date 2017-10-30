7c81330d836e737000dc818081d56bea584d2224
Kyle Lin <kylejlin.us@gmail.com>
2017-10-29 20:23:36 -0700

    Built react-app

---
+++ react-app/src/TreechatUglyInterface.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './TreechatUglyInterface.css';
+
+class TreechatUglyInterface extends Component {
+  render() {
+    return (
+      <div className="TreechatUglyInterface">
+        <h1>Testing React app</h1>
+      </div>
+    );
+  }
+}
+
+export default TreechatUglyInterface;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TreechatUglyInterface from './TreechatUglyInterface';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TreechatUglyInterface />, document.getElementById('root'));
 registerServiceWorker();
